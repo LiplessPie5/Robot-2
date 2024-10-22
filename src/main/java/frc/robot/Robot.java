708ec8503e3f83de5b4bf573941e59e6ed5dd8db
LiplessPie5 <efaulkner25@riverdale.k12.or.us>
2024-10-22 12:00:06 -0700

    updated robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,6 +23,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    enableLiveWindowInTest(true);
   }
   /**
    * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
